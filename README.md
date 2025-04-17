7dc430a479d7773c2e342592ba7d5f8f80db0509
linmengmeng <1632284989@qq.com>
2025-04-17 13:43:09 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-learning-demo
+记录一些Java学习小 demo
---
+++ src/main/java/com/lin/Main.java
@@ -0,0 +1,11 @@
+package com.lin;
+
+/**
+ * @author linmengmeng
+ * @since 2025/4/17 11:37
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
